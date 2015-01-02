addb105af383a861231fce0e5a36a7f03119debb
Rolf Erik Lekang <me@rolflekang.com>
2015-01-02 18:36:22 +0100

    init

---
+++ README.rst
@@ -0,0 +1,8 @@
+frigg-coverage
+==============
+
+A utility package for reading coverage reports. Used by
+frigg-worker to detect coverage from test runs.
+
+
+    pip install frigg-coverage
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='frigg-coverage',
+    version='0.1.0',
+    description='Coverage report reading util',
+    author='The frigg team',
+    author_email='hi@frigg.io',
+    license='MIT',
+    url='https://github.com/frigg/frigg-coverage',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=[
+        'pyyaml',
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python 3',
+    ]
+)
