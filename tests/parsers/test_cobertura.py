a0ffa3ff42e7707e360d5fb297bfbc9fb240cf1c
Rolf Erik Lekang <me@rolflekang.com>
2015-04-13 11:01:30 +0200

    Remove support for reading file in parse_coverage
    parse_coverage now requires the input to be a string containing the
    coverage report.

--- tests/parsers/test_cobertura.py
+++ tests/parsers/test_cobertura.py
@@ -3,5 +3,5 @@ from frigg_coverage import parse_coverage
 
 
 def test_parse_coverage_report():
-    assert parse_coverage('fixtures/cobertura.xml', 'cobertura') == 88.24
-    assert parse_coverage('bad-path', 'cobertura') is None
+    with open('fixtures/cobertura.xml') as f:
+        assert parse_coverage(f.read(), 'cobertura') == 88.24
