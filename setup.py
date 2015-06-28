490de71a36f1fba4f52ba19a3fecee63f2b21c41
Rolf Erik Lekang <me@rolflekang.com>
2015-06-28 20:01:31 +0200

    1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='frigg-coverage',
-    version='1.0.0',
+    version='1.1.0',
     description='Coverage report reading util',
     author='The frigg team',
     author_email='hi@frigg.io',
